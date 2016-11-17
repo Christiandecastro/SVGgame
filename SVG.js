a0440b29052cc60d3d1d3938233b4190b6c14e94
Christian De Castro <robert12102000@yahoo.com>
2016-11-17 11:26:21 -0800

    added food
    added health packs for the character to collect

--- SVG.js
+++ SVG.js
@@ -1,13 +1,25 @@
-var megaman = 375;
+var megamanX = 375;
+var megamanY  = 0;
+
 document.addEventListener("keydown", function(e) {
 if(e.keyCode == 37){
 
-document.getElementById("megamans").setAttribute("x", megaman - 15)
-  megaman = megaman - 15;
+document.getElementById("megamans").setAttribute("x", megamanX - 15)
+  megamanX = megamanX - 15;
 }
 else if(e.keyCode == 39){
 
-document.getElementById("megamans").setAttribute("x", megaman + 15)
-  megaman = megaman + 15;
+document.getElementById("megamans").setAttribute("x", megamanX + 15)
+  megamanX = megamanX + 15;
 }
+
+  else if(e.keyCode == 38) {
+    document.getElementById("megamans").setAttribute("y", megamanY - 15)
+    megamanY = megamanY - 15;
+  }
+
+  else if(e.keyCode == 40) {
+    document.getElementById("megamans").setAttribute("y", megamanY + 15)
+    megamanY = megamanY + 15;
+  }
 })
