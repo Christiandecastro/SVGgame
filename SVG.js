79b484880068eea476a069415a6146fee0f399a8
Christian De Castro <robert12102000@yahoo.com>
2016-12-01 10:47:42 -0800

    added overlap eating

--- SVG.js
+++ SVG.js
@@ -1,5 +1,15 @@
 var megamanX = 375;
 var megamanY  = 0;
+var foodEaten = 0;
+
+var health1 = 250;
+var health2 = 650;
+
+// randomNumber returns a random number between min and max
+function randomNumber(min,max)
+{
+ return Math.floor(Math.random()*(max-min+1)+min);
+}
 
 document.addEventListener("keydown", function(e) {
 if(e.keyCode == 37){
@@ -22,4 +32,23 @@ document.getElementById("megamans").setAttribute("x", megamanX + 15)
     document.getElementById("megamans").setAttribute("y", megamanY + 15)
     megamanY = megamanY + 15;
   }
+
+  var health1Y = Number(document.getElementById("health1").getAttribute("y"))
+  var health1X = Number(document.getElementById("health1").getAttribute("x"))
+  var health2Y = Number(document.getElementById("health2").getAttribute("y"))
+  var health2X = Number(document.getElementById("health2").getAttribute("x"))
+
+
+  if (megamanX > health1X && megamanY < health1Y + 80 && megamanY > health1Y && megamanY < health1Y + 80) {
+    console.log("overlap")
+    var randX = randomNumber(50,750)
+    document.getElementById("health1").setAttribute("x", randX)
+    foodEaten = foodEaten + 1
+    document.getElementById("score").textContent = foodEaten
+
+
+  }
+
+
+
 })
